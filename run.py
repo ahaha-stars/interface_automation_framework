95b6dc5f4f77ffa959a29303c2ffb98fc987d6fe
ahaha-stars <2810368287@qq.com>
2025-10-16 11:55:15 +0800

    push演示

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import pytest
 import shutil
 
 if __name__ == '__main__':
+    #注释
     pytest.main(['-vs','./testcase','--alluredir=./report/temp','--clean-alluredir'])
     shutil.copy('./environment.xml','./report/temp')
     os.system(f'allure serve ./report/temp')
