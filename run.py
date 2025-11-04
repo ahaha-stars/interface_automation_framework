198ecf607def93ace1f78d652eb33c4009360760
ahaha-stars <2810368287@qq.com>
2025-11-04 23:38:17 +0800

    Jenkins服务不自动启动allure服务

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import pytest
 import shutil
 
 if __name__ == '__main__':
-    pytest.main(['-vs','./testcase','--alluredir=./report/temp','--clean-alluredir'])
+    pytest.main(['-vs','./testcase','--alluredir=./report/temp',
+                 '--clean-alluredir'])
     shutil.copy('./environment.xml','./report/temp')
-    os.system(f'allure serve ./report/temp')
+    #启动allure服务命令，Jenkins服务时要关闭
+    #os.system(f'allure serve ./report/temp')
